a0b5194390b928586f6663f0bdb2df42e65c5b0b
Lee Sang-Min <sangmin4208@gmail.com>
2022-01-11 14:15:40 +0900

    불필요한 파일 정리, prettier설정, deploy bash 추가

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+git checkout gh-page
+yarn run build
+cd dist
+git init 
+git add -A 
+git commit -m 'New Deployment'
+git push git@github.com:sangmin4208/sangmin4208.github.io.git main:gh-page -f
+cd .. 
+git checkout main -f
+git branch -D gh-page
